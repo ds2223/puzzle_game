afc46895c37632449637a1b53dbf1015fe98bf35
ShiroJun <0909robinson@gimail.com>
2016-03-15 20:52:56 +0900

    suzuki v2

---
+++ Assets/Suzuki/Collect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Collect : MonoBehaviour {
+    public Text no;
+    public Text nekoName;
+    public Text rank;
+    public Text count;
+    public Text memo;
+
+    void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+    public void View()
+    {
+
+    }
+}
