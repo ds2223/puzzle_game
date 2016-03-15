591223edd5213c7d900fb53f77b6da9006e05370
nemotoyoshiyuki <x0703x@gmail.com>
2016-03-15 20:57:20 +0900

    nemoto v2

---
+++ Assets/nemoto/Script/Destroy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour
+{
+    private bool maguro;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        maguro = GetComponent<Collider2D>().gameObject.tag == "maguro";
+        if (maguro == maguro)
+        {
+            Debug.Log("マグロおいしい");
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/nemoto/Script/Puzzle_board.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Puzzle_board : MonoBehaviour
+{
+
+    //8*8のゲームボードを作るので縦の段を8、横の列を8
+    public int columns = 8;
+    public int rows = 8;
+
+    public GameObject[] Tiles;
+
+    //オブジェクトの位置情報を保存する変数
+    private Transform boardHolder;
+
+    void PuzzleSetup()
+    {
+        //Boardというオブジェクトを作成し、transform情報をboardHolderに保存
+        boardHolder = new GameObject("Board").transform;
+        //x = -1〜8をループ
+        for (int x = 0; x < columns; x++)
+        {
+            //y = -1〜8をループ
+            for (int y = 0; y < rows; y++)
+            {
+                //床をランダムで選択
+                GameObject toInstantiate = Tiles[Random.Range(0, Tiles.Length)];              
+                //床or外壁を生成し、instance変数に格納
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f),
+                    Quaternion.identity) as GameObject;
+                //生成したinstanceをBoardオブジェクトの子オブジェクトとする
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    public void Start()
+    {
+        PuzzleSetup();
+    }
+}
